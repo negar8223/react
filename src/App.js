77e29e96a628bae0a236fc88d322bb09c8310236
Negar Rahmani <negar.negar8223@gmail.com>
2021-10-24 18:26:21 +0330

    add useEffects and callback

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React from "react";
-import {useState} from 'react'
+import { useState } from "react";
+import { useCallback } from "react";
+import { useEffect } from "react";
 
 function Counter() {
+ 
   const [count, setCount] = useState(0);
-   const increas = ()=>{
-     setCount (previuse =>{
-       return previuse + 1
-     })
-     console.log({increaseCount:count})
-   } 
-  
-   const decreas = ()=>{
-    setCount (previuse => {
-      return previuse - 1
-    })
-    console.log({decreaseCount:count})
-  } 
+  useEffect(() => {
+    console.log({ count });
+  }, [count]);
+  const increas = useCallback(() => {
+    setCount((pre) => pre + 1);
+  }, [count]);
+  const decreas = useCallback(() => {
+    setCount((pre) => pre - 1);
+  });
 
-  const reset = ()=>{
-    setCount (0)
-    console.log('reset')
-  }
-  return(
+  const reset = useCallback(() => {
+    setCount(0);
+  }, [count]);
+  return (
     <>
-    <h1>count :  {count}</h1>
-       
-       <button onClick={decreas}>-</button>
+      <h1>count : {count}</h1>
+
+      <button onClick={decreas}>-</button>
       <button onClick={increas}>+</button>
       <button onClick={reset}>reset</button>
     </>
-    )
-    }
+  );
+}
 export default Counter;
